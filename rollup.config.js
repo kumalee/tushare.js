c4b5533cd0fc2a279e4603d5694de72999327240
Kuma Li <li.jun.kuma@gmail.com>
2021-09-05 22:17:32 +0800

    first commit

---
+++ rollup.config.js
@@ -0,0 +1,36 @@
+import { terser } from "rollup-plugin-terser";
+import resolve from "@rollup/plugin-node-resolve";
+import babel from "@rollup/plugin-babel";
+import commonjs from "@rollup/plugin-commonjs";
+import { name } from "./package.json";
+
+export default {
+    input: "src/tushare.js",
+    external: [
+        "axios",
+        "@babel/runtime",
+    ],
+    plugins: [
+        resolve(),
+        babel({
+            exclude: "node_modules/**",
+            babelHelpers: "runtime",
+        }),
+        commonjs({
+            include: "node_modules/**",
+        }),
+        terser(),
+    ],
+    output: [
+        {
+            file: "tushare.js",
+            format: "umd",
+            name,
+            sourcemap: true,
+        },
+        {
+            file: "tushare.esm.js",
+            format: "es",
+        },
+    ],
+};
---
+++ src/tushare.js
@@ -0,0 +1,72 @@
+/* eslint-disable camelcase */
+import axios from "axios";
+import { api } from "./const";
+
+/**
+ *
+ * @param {string} apiName
+ * @param {object} params
+ * @param {array} fields
+ */
+// eslint-disable-next-line max-params
+const query = async ({
+  apiName = "",
+  params = {},
+  fields = [],
+}) => {
+  if (!api && api === "") {
+    throw new Error("API is required");
+  } else if (!api[apiName]) {
+    throw new Error("API not Found");
+  }
+
+  try {
+    const response = await axios.post(api.url, {
+      api: apiName,
+      token: process.env.TUSHARE_TOKEN,
+      params,
+      fields: fields,
+    });
+    if (response && response.data && response.data.code === 0) {
+      let fields = response.data.data.fields;
+      let items = response.data.data.items;
+      let hasMore = response.data.data.has_more;
+
+      let data = await formatData({ fields, items });
+
+      return [
+        true,
+        data,
+        hasMore,
+      ];
+    } else {
+      return [false]
+    }
+  } catch (error) {
+    throw error;
+  }
+}
+
+/**
+ * format data to an array like [{field: value}]
+ * @param {Array} data
+ */
+const formatData = async (data) => {
+    if (!data || data.length <= 0) return data;
+    let fields = data.fields;
+    let items = data.items;
+    let result = [];
+    if (!fields || fields.length === 0 || !items || items.length === 0) {
+        return result;
+    }
+    for (let itemIndex = 0; itemIndex < items.length; itemIndex++) {
+        let newItem = {};
+        for (let fieldIndex = 0; fieldIndex < fields.length; fieldIndex++) {
+            newItem[fields[fieldIndex]] = items[itemIndex][fieldIndex];
+        }
+        result.push(newItem);
+    }
+    return result;
+}
+
+export default query;
