d914a0e1e904cf6a8dd4f2fdb1c05e3343df1e61
Kuma Li <li.jun.kuma@gmail.com>
2021-09-14 10:47:14 +0800

    add es6 import support for jest

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,9 @@
 module.exports = {
   preset: 'ts-jest',
   testEnvironment: 'node',
+  coverageDirectory: "coverage",
+  transform: {
+    "^.+\\.[tj]s?$": "babel-jest", // Adding this line solved the issue
+    "^.+\\.[tj]s?$": "ts-jest"
+  },
 };
--- test/stock_basic.test.js
+++ test/stock_basic.test.js
@@ -1,8 +1,8 @@
-const TuShare = require('../dist/tushare.js');
-const dotenv = require('./token');
+import TuShare from '../src/main';
+import './token';
 
 test('stock_basic', async () => {
-  const t = new TuShare(dotenv.parsed['token']);
-  const result = await t.stock_basic();
+  const t = new TuShare(process.env.token);
+  const result = await t.stock_basic({limit: 5, offset: 0});
   expect(result.isSuccess).toBe(true);
 });
